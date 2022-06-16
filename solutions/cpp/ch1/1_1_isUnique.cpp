7e1dcf1c28ebb21202d8c505538934353198e318
Julio Angulo <solojoas@gmail.com>
2022-06-16 23:20:59 +0000

    finished exercise 1.1!

--- solutions/cpp/ch1/1_1_isUnique.cpp
+++ solutions/cpp/ch1/1_1_isUnique.cpp
@@ -1,15 +1,55 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
-bool isUnique(std::string msg){
-    return false;
+bool isUnique(const std::string& msg){
+    std::unordered_map<char, int> charCount;
+
+    for(char c: msg)
+    {
+        if(charCount.find(c) != charCount.end())
+        {
+            charCount[c]++;
+        }
+        else
+        {
+            charCount[c] = 1;
+        }
+    }
+
+    for(auto [character, count]: charCount)
+    {
+        if(count>1) return false;
+    }
+
+    return true;
+}
+
+bool test_isunique(const std::string& msg)
+{
+    bool result = isUnique(msg);
+    std::cout   <<"'"<< msg << "'" << " does contains unique characteres? "
+                << (result ? "true" : "false") << std::endl;
+
+    return result;
 }
 
 int main()
 {
     std::cout << "Function to determine if an string contain unique characteres\n";
 
-    std::cout   << "abcde does contains unique characteres? "
-                << isUnique("abcde") << std::endl;
+    std::vector<std::string> testMsgs{
+        "a",
+        "aa",
+        "abc",
+        "monsse",
+        "julio"
+        };
+
+    for(const auto& msg: testMsgs)
+    {
+        test_isunique(msg);
+    }
     return 0;
 }
