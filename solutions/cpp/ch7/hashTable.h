7bc439f3fc1268eb88e7332277d1dc4758f6f5e9
Julio Angulo <solojoas@gmail.com>
2022-10-24 20:05:23 +0000

    Continuing with hashtable implementatin...

--- solutions/cpp/ch7/hashTable.h
+++ solutions/cpp/ch7/hashTable.h
@@ -1,7 +1,60 @@
 #include <list>
-#include<vector>
+#include <vector>
+#include <functional>
+#include <iostream>
 
 template <typename Key, typename Value>
 class HashTable{
+public:
+    HashTable();
+    void add(Key key, Value value);
+    void print();
+private:
+    struct Element{
+        Key key;
+        Value value;
+    };
 
+    using ChainList=std::list<Element>;
+    std::vector<ChainList> mTable;
+
+    const double duplicationRatio = 0.7;
+    int mDivitionRatio = 8;
+    double porcentageUsage();
 };
+
+template <typename Key, typename Value>
+HashTable<Key, Value>::HashTable():mTable(8){}
+
+template <typename Key, typename Value>
+void HashTable<Key, Value>::add(Key key, Value value) {
+    auto hash = std::hash<Key>{}(key);
+
+    if(porcentageUsage() > duplicationRatio) {
+        // duplicate table size
+        mTable.reserve(mTable.capacity()*2);
+        mDivitionRatio *= 2;
+    }
+
+    std::size_t tableIndex = hash / (mDivitionRatio - 1);
+    mTable.at(tableIndex).push_back(Element{key, value});
+}
+
+template <typename Key, typename Value>
+void HashTable<Key, Value>::print() {
+    std::cout << "{ ";
+
+    for(const auto& chainList : mTable) {
+        for(const auto& elem : chainList) {
+            std::cout << elem.key << " : " << elem.value << ", ";
+        }
+    }
+
+    std::cout << "}" << std::endl;
+}
+
+
+template <typename Key, typename Value>
+double HashTable<Key, Value>::porcentageUsage() {
+    return (double)mTable.size()/(double)mTable.capacity();
+}
--- solutions/cpp/ch7/hashTableTest.cpp
+++ solutions/cpp/ch7/hashTableTest.cpp
@@ -4,5 +4,13 @@
 int main() {
     HashTable<std::string, int> dict;
 
+    dict.add("monday", 1);
+    dict.add("tuesday", 2);
+    dict.add("wednesday", 3);
+    dict.print();
+
+    HashTable<char, int> dictAlphabet;
+    
+
     return 0;
 }
