a21444cd8d912f30eae43050416c9a0197dd90c9
Julio Angulo <solojoas@gmail.com>
2022-12-21 00:06:15 +0000

    Added methods to circularArray
    * Added size method
    * overrided the operator[ ] for circularArray

--- solutions/cpp/ch7/7_9_circularArrayTest.cpp
+++ solutions/cpp/ch7/7_9_circularArrayTest.cpp
@@ -1,4 +1,17 @@
 #include "circularArray.h"
+#include <iostream>
+
+template<typename T, std::size_t Size>
+void testCircArrayConstruction(CircularArray<T, Size>& circArr){
+    std::cout << "*******************************" << std::endl;
+    for(std::size_t i=0; i<circArr.size(); i++){
+       std::cout << "circular Array["<<i<<"] = "<< circArr[i] << std::endl; 
+    }
+
+    std::cout << "Testing size() method..." << std::endl;
+    std::cout << "circArr.size() = " << circArr.size() << std::endl;
+    std::cout << "*******************************" << std::endl;
+}
 
 int main()
 {
@@ -7,5 +20,12 @@ int main()
     for(std::size_t i=0; i<10; i++){
         circArr[i] = 2*i;    
     }
+
+    std::cout << "Testing operator [] ..." <<std::endl;
+    testCircArrayConstruction(circArr);
+    // Testing initializer list
+    CircularArray<int, 10> initListArr{{1,2,3,4,5,6,7,8,9,10}};
+    testCircArrayConstruction(initListArr);
+
     return 0;
 }
--- solutions/cpp/ch7/circularArray.h
+++ solutions/cpp/ch7/circularArray.h
@@ -1,9 +1,29 @@
 #include <cstddef>
 #include <array>
+#include <initializer_list>
+#include <algorithm>
+#include <iterator>
 
 template <typename Type, std::size_t Size>
 class CircularArray{
 public:
+    CircularArray(){}
+
+    CircularArray(std::initializer_list<Type> list){
+        std::copy(list.begin(), list.end(), data.begin());
+    }
+
+    Type& operator[](const std::size_t index){
+        return data[index];
+    }
+
+    std::size_t size(){
+        return data.size();
+    }
+
+    void rotate(std::size_t positions){
+        
+    }
 
 
 private:
